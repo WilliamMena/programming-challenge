85f986738614136595047592d179d443d520f13f
WilliamMena <williammena1991@gmail.com>
2018-04-29 00:05:04 -0400

    Replace 'chart' gem with 'chartkick'

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,8 +13,3 @@
 //= require rails-ujs
 //= require turbolinks
 //= require_tree .
-//= require chart
-//= require chart2 (not compatible with chart 1.1.1)
-//= require nvd3
-//= require google
-//= require highcharts
--- app/controllers/video_controller.rb
+++ app/controllers/video_controller.rb
@@ -39,6 +39,43 @@ class VideoController < ApplicationController
 
   def show
     @video = Video.find(params[:id])
+    @lineSize = {
+      :height => 500,
+      :width => 500
+    }
+    @lineData = [
+        {
+          value: 300,
+          color:"#F7464A",
+          highlight: "#FF5A5E",
+          label: "Red"
+        },
+        {
+          value: 50,
+          color: "#46BFBD",
+          highlight: "#5AD3D1",
+          label: "Green"
+        },
+        {
+          value: 100,
+          color: "#FDB45C",
+          highlight: "#FFC870",
+          label: "Yellow"
+        },
+        {
+          value: 40,
+          color: "#949FB1",
+          highlight: "#A8B3C5",
+          label: "Grey"
+        },
+        {
+          value: 120,
+          color: "#4D5360",
+          highlight: "#616774",
+          label: "Dark Grey"
+        }
+
+      ].to_json
   end
 end
 
