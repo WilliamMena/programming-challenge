af3c3ccbaf619046c640c8782afc5f1455397473
WilliamMena <williammena1991@gmail.com>
2018-04-28 22:28:32 -0400

    Import Faker gem so when creating new User for the Video, it can have Random Names

--- app/controllers/video_controller.rb
+++ app/controllers/video_controller.rb
@@ -5,25 +5,31 @@ class VideoController < ApplicationController
   end
 
   def create
-    binding.pry
     random_time = Random.rand(100..600)
     @video = Video.new(video_params)
     @video.length = Time.at(random_time).utc
-    @video.VideoUsage.create()
+
+    if @video.save
+      @video.VideoUsage.create()
+
+      # pick a random number for number of views on the specific video
+      x = Random.rand(1..10)
+      while x >= 0
+        @video.VideoUsage.last.user = (User.create(:name => Faker::Name.first_name, :age => Random.rand(18..60)))
+        @video.VideoUsage.last.createTime
+        @video.VideoUsage.last.save
+        x -= 1
+      end
 
 
-    # pick a random number for number of views on the specific video
-    x = Random.rand(1..10)
-    while x >= 0
-      @video.VideoUsage.last.user = (User.create(:name => "William", :age => Random.rand(18..60)))
-      @video.VideoUsage.last.createTime
-      @video.VideoUsage.last.save
-      x -= 1
+
+      redirect_to video_path(@video)
+    else
+      redirect_to new_video_path
+      flash.alert = @list.errors.full_messages[0]
     end
 
 
-
-    redirect_to video_path(@video)
   end
 
   def index
@@ -38,6 +44,6 @@ end
 
 private
 
-  def video_params
-    params.permit(:video_id)
-  end
+def video_params
+  params.permit(:video_id)
+end
